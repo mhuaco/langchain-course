4e9626cce926156a0efc8e5a4a1890087bc36563
mhuaco <marvin.huaco@yahoo.com>
2026-01-03 18:39:11 -0500

    intro to search agents with pydantic and typing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,9 +24,20 @@ from langchain_tavily import TavilySearch
 #     print(f"Searching for {query}")
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for a source used by the agent """
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for the response with answer and sources"""
+
+    answer:str = Field(description="The answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 llm = ChatOpenAI(model="gpt-5")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
