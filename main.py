3dbbaefcd3c2d51331cd91daac29c5139c3f531e
mhuaco <marvin.huaco@yahoo.com>
2026-01-07 00:41:07 -0500

    react search agent

---
+++ main.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt,
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+chain = agent_executor
+
+def main():
+    result = chain.invoke(
+        input={
+            "input": "search for 3 remote jobs postings for an ai engineer using langchain on linkedin and list their details"
+        }
+    )
+
+if __name__ == "__main__":
+    main()
+
